2dda6103c27b8689c2561ac44b9523cc72d463f1
Bogdan Ermurachi <110774389+bogdanerm@users.noreply.github.com>
2023-10-28 09:22:06 +0300

    Create keyboard.go

---
+++ keyboard/keyboard.go
@@ -0,0 +1,25 @@
+// Package keyboard gets input from keyboard
+package keyboard
+
+import (
+	"bufio"
+	"os"
+	"strconv"
+	"strings"
+)
+
+// GetFloat gets float value from keyboard
+func GetFloat() (float64, error) {
+	reader := bufio.NewReader(os.Stdin)
+	input, err := reader.ReadString('\n')
+	if err != nil {
+		return 0, err
+	}
+
+	input = strings.TrimSpace(input)
+	number, err := strconv.ParseFloat(input, 64)
+	if err != nil {
+		return 0, err
+	}
+	return number, nil
+}
